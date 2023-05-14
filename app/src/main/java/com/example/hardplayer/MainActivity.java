2720fc3e8b388f03fdca5aac55e231c89db9bc1c
wennerryle(Vladislav Pozdeev) <wennerryle@gmail.com>
2023-05-15 01:40:22 +0300

    [feat]: add permissions check, with popups

--- app/src/main/java/com/example/hardplayer/MainActivity.java
+++ app/src/main/java/com/example/hardplayer/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.hardplayer;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.motion.widget.MotionLayout;
 import androidx.constraintlayout.motion.widget.TransitionAdapter;
@@ -7,10 +9,15 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewpager2.widget.ViewPager2;
 
+import android.Manifest;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.hardplayer.data.Playlist;
 import com.example.hardplayer.ui.components.playlistcarousel.RecycleViewPlaylistAdapter;
@@ -21,7 +28,14 @@ import com.google.android.material.tabs.TabLayoutMediator;
 import java.util.ArrayList;
 
 import og.android.lib.toggleiconview.ToggleIconView;
+import permissions.dispatcher.NeedsPermission;
+import permissions.dispatcher.OnNeverAskAgain;
+import permissions.dispatcher.OnPermissionDenied;
+import permissions.dispatcher.OnShowRationale;
+import permissions.dispatcher.PermissionRequest;
+import permissions.dispatcher.RuntimePermissions;
 
+@RuntimePermissions
 public class MainActivity extends AppCompatActivity {
     float playerAnimationProgress = 0;
     CheckBox favoriteCheckbox;
@@ -60,6 +74,8 @@ public class MainActivity extends AppCompatActivity {
 
         new TabLayoutMediator(findViewById(R.id.main_screen_tab_layout),
                 viewPager,
+                false,
+                true,
                 ((tab, position) -> tab.setText(position == 0 ? "Избранные" : "Все" ))
         ).attach();
 
@@ -84,5 +100,48 @@ public class MainActivity extends AppCompatActivity {
                 backTimerCollapsed.post(backTimerAnimation::run);
             }
         });
+
+        MainActivityPermissionsDispatcher.requestStorageWithPermissionCheck(this);
+    }
+
+    @NeedsPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+    void requestStorage() {
+        System.out.println("It's work!");
+    }
+
+    @OnShowRationale(Manifest.permission.READ_EXTERNAL_STORAGE)
+    void showRationaleForStorage(final PermissionRequest request) {
+        new AlertDialog.Builder(this)
+                .setTitle("Отсутствует разрешение!")
+                .setMessage("Этот аудиоплеер использует память устройства, для поиска в нём аудио.")
+                .setPositiveButton("Хорошо", (dialog, button) -> request.proceed())
+                .setNegativeButton("Неа", (dialog, button) -> request.cancel())
+                .show();
+    }
+
+    @OnPermissionDenied(Manifest.permission.READ_EXTERNAL_STORAGE)
+    void showDeniedForStorage() {
+        Toast.makeText(this, "Разрешите приложение доступ к файлам!", Toast.LENGTH_SHORT).show();
+        showSettingsOfApp();
+    }
+
+    @OnNeverAskAgain(Manifest.permission.READ_EXTERNAL_STORAGE)
+    void showNeverAskForStorage() {
+        Toast.makeText(this, "Разрешите приложение доступ к файлам!", Toast.LENGTH_SHORT).show();
+        showSettingsOfApp();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        MainActivityPermissionsDispatcher.onRequestPermissionsResult(this, requestCode, grantResults);
+    }
+
+    public void showSettingsOfApp() {
+        Intent intent = new Intent();
+        intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+        Uri uri = Uri.fromParts("package", getPackageName(), null);
+        intent.setData(uri);
+        startActivity(intent);
     }
 }
